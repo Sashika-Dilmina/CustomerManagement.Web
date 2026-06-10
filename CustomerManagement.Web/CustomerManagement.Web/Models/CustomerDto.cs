0515ca30174d40d037132f29e0278fbab9633763
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2026-06-10 10:04:18 +0530

    Dto Models

---
+++ CustomerManagement.Web/CustomerManagement.Web/Models/CustomerDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomerManagement.Web.Models;
+
+public class CustomerDto
+{
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public string CustomerCode { get; set; } = string.Empty;
+    public string? Address { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string CustomerTypeName { get; set; } = "Personal";
+    public string? Email {  get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
---
+++ CustomerManagement.Web/CustomerManagement.Web/Models/CustomerSaveDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerManagement.Web.Models;
+
+public class CustomerSaveDto
+{
+    public string CustomerName { get; set; } = string.Empty;
+    public string? Address { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string CustomerType { get; set; } = "Personal";
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool IsActive { get; set; } = true;
+
+}
