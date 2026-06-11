646e2ad46d2d56e5f002c31436e8ed7c9483dcdf
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2026-06-11 16:18:21 +0530

    final

--- CustomerManagement.Web/CustomerManagement.Web/obj/Debug/net10.0/CustomerManagement.Web.AssemblyInfo.cs
+++ CustomerManagement.Web/CustomerManagement.Web/obj/Debug/net10.0/CustomerManagement.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerManagement.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d739545978b7fd583b39b1e2fa89e8a52188c831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d02425260c430e02e782fc39d39d5ded1d7679")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerManagement.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerManagement.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
